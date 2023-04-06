bcd0c78099ab9693b1ccbf3e887036676200bf49
Peter Trifanov <peter.trifanov@gmail.com>
2023-04-06 12:49:07 +0000

    test

--- parser/impl/impl.go
+++ parser/impl/impl.go
@@ -53,7 +53,7 @@ func (l *DiceNotationListenerImpl) ExitNotation(ctx *parser.NotationContext) {
 	if l.stack.Size() == 0 && ctx.GetText() == "" {
 		log.Println("DEBUG: Attempt to get result when stack is empty, expression is [", ctx.GetText(), "]")
 		return
-	} else if l.stack.Size() == 1 {
+	} else if    l.stack.Size() == 1 {
 		l.result = l.pop()
 	} else {
 		log.Println("DEBUG: Doing nothing; there are multiple results on stack: ", l.stack, ", expression is [", ctx.GetText(), "]")
